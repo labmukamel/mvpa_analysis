e88cc541196976e3363d94f430a97a923c69b727
lshkiller <lshkiller@gmail.com>
2015-08-31 17:47:20 +0300

    pre analysis setup

--- Daniel/pre_analysis.py
+++ Daniel/pre_analysis.py
@@ -3,6 +3,7 @@ import sys
 import os
 import re
 
+from OpenFMRIAnalyzer import OpenFMRIAnalyzer
 from OpenFMRIData import OpenFMRIData
 
 #subname    	= sys.argv[1]
@@ -18,9 +19,10 @@ op = OpenFMRIData(data_dir, raw_dir,study_name)
 subject_names = op.get_subject_names() # All subjects
 
 for name in subject_names:
-    subject_dir = op.create_subject_dir(name)
+    subject_dir = op.load_subject_dir(subname=name)
     # if we want to create new data for analysis
     # subject_dir = op.create_subject_dir(name)
-    #analyzer = OpenFMRIAnalyzer(op,[subject_dir.subcode()])
-    #analyzer.analyze(mc_merge=True)
+    analyzer = OpenFMRIAnalyzer(op,[subject_dir])
+    analyzer.extract_brain(subject_dir)
+    analyzer.estimate_bias_field(subject_dir)
 
