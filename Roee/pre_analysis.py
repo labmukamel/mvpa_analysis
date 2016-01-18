91b5ac317e09663f2c2d5955405abec5b2e5410a
roeegilron <roeegilron@gmail.com>
2016-01-18 12:10:02 +0200

    Changes Pre Analysis Files

--- Roee/pre_analysis.py
+++ Roee/pre_analysis.py
@@ -7,7 +7,7 @@ from OpenFMRIData import OpenFMRIData
 
 #subname    	= sys.argv[1]
 
-data_dir   	= os.environ.get('DATA_DIR') or '/media/roee/fMRI/Data/'
+data_dir   	= os.environ.get('DATA_DIR') or '/home/rack-hezi-03/home/roigilro/fMRI/Data/'
 study_name 	= os.environ.get('STUDY_NAME') or 'Self-Other'
 
 raw_dir 	= os.path.join(data_dir,'raw')
@@ -18,30 +18,36 @@ op = OpenFMRIData(data_dir, raw_dir,study_name)
 subject_names = op.get_subject_names() # All subjects
 
 for name in subject_names:
-    subject = op.load_subject_dir(subname=name)
     # if we want to create new data for analysis
     # subject_dir = op.create_subject_dir(name)
-    analyzer = OpenFMRIAnalyzer(op,[subject])
 
+    # create new data (convert dicom to nii etc.creat
+    # subject = op.load_subject_dir(subname=name)
+    # - Creates the new directories only when given subname!
+    jsonmaping = op.mapping_json()
+    subject_code = jsonmaping[name]
+    # just load existing data structure
+    subject = op.load_subject_dir(subname= name, subcode= subject_code)
+    analyzer = OpenFMRIAnalyzer(op,[subject])
     # Brain Extraction and bias field estimation
     print "Started Analysis:{}".format(subject)
-    brain_image = analyzer.extract_brain(subject,automatic_approval=True)
-    anat_image = analyzer.estimate_bias_field(subject, brain_image)
+    # brain_image = analyzer.extract_brain(subject,automatic_approval=False)
+    # anat_image = analyzer.estimate_bias_field(subject, brain_image)
 
-    # Motion Correction
+    # Motion
     analyzer.motion_correction(subject)
 
-    # Slice‐time correction
-    # analyzer.slice_time_correction(subject)
+    # Slice‐time correction - test
+    analyzer.slice_time_correction(subject)
 
     # Spatial  filtering  ( Smoothing)
-    analyzer.anatomical_smoothing(subject,8.0,2000.0)
+    # analyzer.anatomical_smoothing(subject,8.0,2000.0)
     # analyzer.functional_smoothing(subject,kwargs['fwhm'],kwargs['brightness_threshold'])
 
     # Temporal  filtering (High Pass)
 
     # Anatomical Registration
-    analyzer.anatomical_registration(subject)
+    # analyzer.anatomical_registration(subject)
 
     # Functional Registration
     analyzer.functional_registration(subject)
